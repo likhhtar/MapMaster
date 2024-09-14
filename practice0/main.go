d65494479d61460de261d73e1dda873e37c59b57
Anna <annalikhtar5@mail.ru>
2024-09-14 16:42:45 +0300

    done

--- practice0/main.go
+++ practice0/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func main() {
+	b := make([]byte, 100)
+	go func() {
+		for {
+			filler(b[:50], '0', '1')
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			filler(b[50:], 'X', 'Y')
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			fmt.Println(string(b))
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for {
+		time.Sleep(10 * time.Second)
+	}
 }
 
 func filler(b []byte, ifzero byte, ifnot byte) {
